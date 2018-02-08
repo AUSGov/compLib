56df8b6662ba612d4129ff8472b45315b963a0e3
jsephora <joey@sephoradesign.com>
2018-02-08 16:21:13 +1100

    updated buttons and footer

--- src/views/Buttons/Buttons.js
+++ src/views/Buttons/Buttons.js
@@ -6,11 +6,23 @@ class Buttons extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
-          <div className="standard-header">
-            <h1>Buttons</h1>
-            <div className="version">Last update: 07/02/2018</div>
-          </div>          
-        </Row>
+          <Col>
+            <div className="standard-header">
+              <h1>Buttons</h1>
+              <div className="version">Last update: 07/02/2018</div>
+            </div>
+          </Col>
+        </Row> 
+        <Row>
+          <Col>            
+            <div className="bodyText">
+              <p>Use buttons and links to trigger actions. Buttons can contain a combination of a clear label and an <a href="/icons">icon</a> while links are always text.</p>
+              <h2>Button types</h2>
+              <h3>Primary button</h3>
+              <Button color="primary">primary</Button>{' '}
+            </div>
+          </Col>
+        </Row>          
       </div>
     )
   }
