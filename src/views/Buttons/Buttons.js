045d59baa67dc7216b1f1cbdbd7f59f898eb0e21
jsephora <joey@sephoradesign.com>
2018-02-27 16:43:02 +1100

    updated buttons page

--- src/views/Buttons/Buttons.js
+++ src/views/Buttons/Buttons.js
@@ -17,6 +17,7 @@ class Buttons extends Component {
             <div className="content">
               <p className="intro">A wide range of button styles are used across the website. Appropriate button styles are to be used based on context and level of importance.</p>
 
+{/* -- Primary buttons -- */}
               <h2>Primary button styles</h2>
               <p>Use of primary buttons is reserved for for the primary action on the page. For example, a page that encourages users to give feedback would reserve the primary button for a ‘Message us’ action.</p>
               
@@ -34,6 +35,7 @@ class Buttons extends Component {
               </div>
               <hr></hr>
 
+{/* -- Default buttons -- */}
               <h2>Default button styles</h2>
               <p>Default button styles are used for strong call to actions but sit on a level below primary button styles in terms of importance.</p>
 
@@ -50,8 +52,24 @@ class Buttons extends Component {
                   <br/>
                   <Button className="bga-btn btn-default btn-lg btn-block">Full width button</Button>
               </div>
+              <hr></hr>
+
+{/* -- Tertiary buttons -- */}
+              <h2>Tertiary button styles</h2>
+              <p>Tertiary buttons have a lower level of importance than default button styles and are also ideal for when longer lines of copy are needed.</p>
+
+              <div className="demo-block">
+                <Button className="bga-btn btn-tertiary"><i class="fa fa-arrow-right left"></i>Find out more</Button>{' '}
+              </div>
 
               <hr></hr>
+              
+              <h3>Variations</h3>
+              <div className="demo-block">
+                  <Button className="bga-btn btn-tertiary"><i class="fa fa-arrow-right left"></i>Find out more</Button>{' '}
+              </div>
+              <hr></hr>
+
 
             </div>
           </Col>
